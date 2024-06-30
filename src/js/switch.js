7651cb0e07c56294afc0979ffde8d903a6a093ff
Daniel Dantas <daniel.dantas808@gmail.com>
2024-06-30 17:04:45 -0300

    feat: parte de preferencias feita

--- src/js/switch.js
+++ src/js/switch.js
@@ -1,12 +1,6 @@
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-    const toggleSwitch = document.getElementById('customSwitch1');
 
-    toggleSwitch.addEventListener('change', function() {
-      if (this.checked) {
-        console.log("Switch is ON");
-      } else {
-        console.log("Switch is OFF");
-      }
-    });
-  });
+  const themeSwitcher = document.querySelector(".theme-switcher")
+  themeSwitcher.addEventListener("click", function (){
+    console.log("estou funcionando")
+  })
