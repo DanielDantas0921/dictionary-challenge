3740431614c48f10587cc2f5f64622ae58d3fab5
Daniel Dantas <daniel.dantas808@gmail.com>
2024-07-02 16:09:57 -0300

    feat: boa parte do javascript feita para montar os elementos

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,35 @@ import {apiWordSearchFunction} from "./apiFunction.js"
 import {makeElements} from "./makeElements.js"
 
 
-apiWordSearchFunction("hello").then(result => makeElements(result))
+// apiWordSearchFunction("hello").then(result => makeElements(result))
 
+// fazer função do input search e do icone
+
+const inputSearch = document.querySelector(".input-search")
+
+inputSearch.addEventListener("keydown", function (event){
+    if(event.key == 'Enter'){
+        event.preventDefault()
+        
+        if(inputSearch.value == ""){
+            return
+        }
+        apiWordSearchFunction(inputSearch.value).then(result => makeElements(result))
+
+
+
+    }
+})
+
+const audioPlay = document.querySelector(".icon-search")
+
+audioPlay.addEventListener("click", (event)=> { 
+    console.log("antes do if")
+        if(inputSearch.value == ""){
+            return
+        }
+
+        console.log("passou")
+        apiWordSearchFunction(inputSearch.value).then(result => makeElements(result))
+
+})
--- src/js/makeElements.js
+++ src/js/makeElements.js
@@ -9,12 +9,19 @@ findTextPhonetic(result[0].phonetics, wordSearchP)
 const audioPlay = document.querySelector(".audio-play")
 const audioAudio = document.querySelector(".audio-audio")
 
-// audioAudio.src = findAudioPhonetic(result[0].phonetics, audioAudio)
-
 findAudioPhonetic(result[0].phonetics, audioAudio)
 audioPlay.addEventListener("click", ()=> audioAudio.play())
 
+// fazer parte do dynamic text da div para baixo
+
+const divDynamicText = document.querySelector("dynamic-text")
+
+makeElementsMeanings(result[0].meanings)
+
+
 }
+
+
 
 function findTextPhonetic(arrayPhonetics, element){
 let textPhonetic = ""
@@ -33,10 +40,63 @@ arrayPhonetics.forEach((item)=>{
 function findAudioPhonetic(arrayPhonetics, elementAudio){
     let srcPhonetic = ""
     arrayPhonetics.forEach((item)=>{
-        console.log(item)
         if(item.audio){
             srcPhonetic = item.audio
             elementAudio.src = srcPhonetic
         }
     })
+}
+
+function makeElementsMeanings(arrayMeanings){
+
+
+    const divDynamicText = document.querySelector(".dynamic-text")
+    divDynamicText.innerHTML = ""
+
+
+    arrayMeanings.forEach((item)=>{
+
+        const divMeaning = document.createElement("div")
+        divMeaning.classList.add("divMeaning")
+
+        const h3PartOfSpeach = document.createElement("h3")
+        h3PartOfSpeach.innerText = item.partOfSpeech
+        h3PartOfSpeach.classList.add("part-of-speech")
+
+        const pMeaning = document.createElement("p")
+        pMeaning.innerText = "Meaning"
+        pMeaning.classList.add("meaning")
+
+        const listaUl = document.createElement("ul")
+
+        item.definitions.forEach((item)=>{
+            const li = document.createElement("li")
+            li.innerText = item.definition
+            listaUl.appendChild(li)
+        })
+
+
+
+        const synonymsText = document.createElement("p")
+        let textSynonyms = " "
+
+        divMeaning.append(h3PartOfSpeach, pMeaning,listaUl)
+
+        if(item.synonyms.length != 0){
+           
+
+
+            item.synonyms.forEach((item)=>{
+                console.log(item)
+                textSynonyms = textSynonyms + item + "; "
+            })
+
+            synonymsText.innerHTML= "<p class='synonymous'>Synonymous:" + textSynonyms + "</p>"
+            divMeaning.appendChild(synonymsText)
+        }
+
+        divDynamicText.appendChild(divMeaning)
+    })
+
+
 }
